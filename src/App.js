042752b3e09178cc6c9fef5c71798c5be8a7edf2
ProninaAnastasiia <soul_bler@mail.ru>
2023-10-29 01:47:19 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navigation />
       </header>
+      <div className="body">
+        <h1>Торговый Дом Любови Власовой</h1>
+        <p>Наша компания по оптовой торговле вяленой и копченой рыбой предлагает покупателям широкий выбор снековой продукции наивысшего качества.</p>
+        <p>Основной ассортимент представлен из Мурманской, Цимлянской и Дальневосточной рыбы.</p>
+      </div>
+      <div className="wrapper">
+        <Card img='/images/Камбала-ерш «По-царски» вяленая.jpg'
+              title='Камбала-ерш «По-царски» вяленая'
+              price='945 ₽/1кг'/>
+        <Card img='/images/Лещ Цимлянский 0,5-0,6 кг вяленый.jpg'
+              title='Лещ Цимлянский вяленый'
+              price='360 ₽/1кг'/>
+        <Card img='/images/Вобла 18+ вяленая.jpg'
+              title='Вобла 18+ вяленая'
+              price='405 ₽/1кг'/>
+      </div>
+      
     </div>
   );
 }
+
+function Navigation() {
+  return (
+    <nav>
+      <ul class="nav-list">
+        <li><a href="#price">Продукция</a></li>
+        <li><a href="#about">О компании</a></li>
+        <li><a href="#contacts">Контакты</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+        <img src={props.img} class="card_image" />
+        <p className="card_title">{props.title}</p>
+        <p className="card_price">{props.price}</p>
+    </div>
+  );
+}
+
 
 export default App;
